66d5baf8d8d329eea5a874db0781e887141584f5
Kiman Park <kimanpark33@gmail.com>
2023-11-29 17:09:10 -0500

    added weather notifications with automatic scheduling

--- Weather.py
+++ Weather.py
@@ -5,110 +5,95 @@ Created on Fri Oct 13 01:12:40 2023
 @author: brian
 """
 import email, smtplib, ssl
-from providers import PROVIDERS
 
 # used for MMS
 from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from providers import PROVIDERS
+from email.parser import Parser
+from email.policy import default
 
-from os.path import basename
 
-def send_sms_via_email(
-    number: str,
+import requests
+from bs4 import BeautifulSoup
+import json
+
+path = "https://weather.com/weather/tenday/l/Knoxville+TN?canonicalCityId=626ef612d09792fa3f39bfb5ad2b6808faf39bd6c597c5d8c7301e27cec2ed4a"
+
+
+def getWeather(path):
+    x = requests.get(path)
+    soup = BeautifulSoup(x.content, 'html5lib')
+                
+    content = []
+    for i in range(20):        
+        content_day = soup.find_all("div",{"data-testid":"DailyContent"})[i]
+        line1 = '------------------------------------------------------' + '\n'
+        
+        time = content_day.find('h3').text
+        line2 = f'{time}\n'
+        
+        
+        temp = content_day.find('span',{'data-testid':'TemperatureValue'}).text
+        line3 = '------' + '\n'
+        line5 = f'Temperature: {temp}'.format(temp=temp) + '\n'
+        
+        precipitation = content_day.find('span',{'class':'DailyContent--value--1Jers'}).text
+        line6 = '------' + '\n'
+        line7 = 'Precipitation percentage (if not, Wind): {precipitation}'.format(precipitation=precipitation) + '\n'
+        
+        summary = content_day.find('p',{'data-testid':'wxPhrase'}).text
+        line9 = '------' + '\n'
+        line10 = 'Summary: {summary}'.format(summary=summary) + '\n'
+
+        line12 = '\n'
+        holy = line1+line2+line3+line5+line6+line7+line9+line10+line12
+        print(holy)
+        content.append(holy)
+    
+    report = ''.join(content).strip()
+    return report
+
+def send_email_via_email(
+    receiver: str,
     message: str,
-    provider: str,
     sender_credentials: tuple,
     subject: str = "sent using etext",
     smtp_server: str = "smtp.gmail.com",
     smtp_port: int = 465,
 ):
     sender_email, email_password = sender_credentials
-    receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'
-
-    email_message = f"Subject:{subject}\nTo:{receiver_email}\n{message}"
-
+    receiver_email = receiver
+        
+    'From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n{}'
+    email_message = f"From: {sender_email}\r\nTo: {receiver}\r\nSubject: {subject}\r\n\r\n{message}".format(sender_email=sender_email,receiver=receiver,subject=subject,message=message)
+    
+    
     with smtplib.SMTP_SSL(
         smtp_server, smtp_port, context=ssl.create_default_context()
     ) as email:
         email.login(sender_email, email_password)
-        email.sendmail(sender_email, receiver_email, email_message)
-
-def send_mms_via_email(
-    number: str,
-    message: str,
-    file_path: str,
-    mime_maintype: str,
-    mime_subtype: str,
-    provider: str,
-    sender_credentials: tuple,
-    subject: str = "sent using etext",
-    smtp_server: str = "smtp.gmail.com",
-    smtp_port: int = 465,
-):
-
-    sender_email, email_password = sender_credentials
-    receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'
-
-    email_message=MIMEMultipart()
-    email_message["Subject"] = subject
-    email_message["From"] = sender_email
-    email_message["To"] = receiver_email
-
-    email_message.attach(MIMEText(message, "plain"))
-
-    with open(file_path, "rb") as attachment:
-        part = MIMEBase(mime_maintype, mime_subtype)
-        part.set_payload(attachment.read())
-
-        encoders.encode_base64(part)
-        part.add_header(
-            "Content-Disposition",
-            f"attachment; filename={basename(file_path)}",
-        )
-
-        email_message.attach(part)
-
-    text = email_message.as_string()
-
-    with smtplib.SMTP_SSL(
-        smtp_server, smtp_port, context=ssl.create_default_context()
-    ) as email:
-        email.login(sender_email, email_password)
-        email.sendmail(sender_email, receiver_email, text)
+        email.sendmail(sender_email, receiver_email, email_message.encode('utf-8'))
 
 
 def main():
-    number = "18652094218"
-    number1 = "18653231401"
-    message = "I do not know why this is not getting through."
-    provider = "T-Mobile"
+    
+    text = getWeather(path)
+
+    receiver = "kimanpark33@gmail.com"
+    receiver1 = "carpenter.abby25@gmail.com"
+    message = text
 
     sender_credentials = ("kimanpark33@gmail.com", "byab pntv aygn eqbo")
+    
+    try:
+        #Email
+        send_email_via_email(receiver, message, sender_credentials,subject='Weather Report')
+        send_email_via_email(receiver1, message, sender_credentials,subject='Weather Report')
+        print('Email Sent!')
 
-    # SMS
-    send_sms_via_email(number, message, provider, sender_credentials)
-    send_sms_via_email(number1, message, provider, sender_credentials)
-    '''
-    # MMS
-    file_path = "/path/to/file/file.png"
-
-    mime_maintype = "image"
-    mime_subtype = "png"
-
-    send_mms_via_email(
-        number,
-        message,
-        file_path,
-        mime_maintype,
-        mime_subtype,
-        provider,
-        sender_credentials,
-    )
-    '''
-
+    except:
+        print('error')        
+    
+    
 if __name__ == "__main__":
     main()
 
